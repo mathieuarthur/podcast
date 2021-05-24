30e8b1f559fffed8b91128abcb87418ace88da7d
mathieuarthur <71327986+mathieuarthur@users.noreply.github.com>
2021-05-24 16:26:07 +0200

    add recursive

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
 <main>
 	<Modal>
 		<Podcast color={"#E423F0"} title={"On m’appelait le fils du diable"} typo={"Patua One"} typoColor={"#FFCF2A"} typoSize={"5em"} />
-		<Podcast color={"#785BCA"} title={"On me comparait à un porcidou"} typo={"arial"} typoColor={"#55EFDB"} typoSize={"5em"}/>
+		<Podcast color={"#785BCA"} title={"On me comparait à un porcidou"} typo={"Recursive"} typoColor={"#55EFDB"} typoSize={"5em"}/>
 		<Podcast color={"#5FC9FA"} title={"AAAAAA"} typo={"Abril Fatface"} typoColor={"#FFFFFE"} typoSize={"5em"}/>
 		<Podcast color={"#E9CECE"} title={"purple"} typo={"Days One"} typoColor={"#C92525"} typoSize={"5em"}/>
 		<Podcast color={"#F4A6D0"} title={"yellow"} typo={"Goblin One"} typoColor={"#0060FF"} typoSize={"5em"}/>
