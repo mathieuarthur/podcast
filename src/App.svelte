cec17558401ba650dd6734f21a4d8939f9318aea
mathieuarthur <71327986+mathieuarthur@users.noreply.github.com>
2021-05-27 23:59:57 +0200

    change podcast 7 title

--- src/App.svelte
+++ src/App.svelte
@@ -14,6 +14,6 @@
 		<Podcast color={"#E9CECE"} title={"Trop maigre à leur yeux"} typo={"Days One"} typoColor={"#C92525"} typoSize={"5em"} clip={"./audio/podcast4.mp3"} />
 		<Podcast color={"#F4A6D0"} title={"Je me suis vengé"} typo={"Goblin One"} typoColor={"#0060FF"} typoSize={"5em"}  clip={"./audio/podcast5.mp3"} />
 		<Podcast color={"#3E4797"} title={"brown"} typo={"Newsreader"} typoColor={"#FF9000"} typoSize={"5em"} clip={"./audio/podcast.mp3"} />
-		<Podcast color={"#C0AAFF"} title={"Mes amis d'enfances m'ont harcelé"} typo={"Orbitron"} typoColor={"#90F2A0"} typoSize={"5em"} clip={"./audio/podcast7.mp3"} />
+		<Podcast color={"#C0AAFF"} title={"Trahison d'amis d'enfance"} typo={"Orbitron"} typoColor={"#90F2A0"} typoSize={"5em"} clip={"./audio/podcast7.mp3"} />
 	</Modal>
 </main>
