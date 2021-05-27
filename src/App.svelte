046b3aad8d34ac4d01da04530773a0aa9267e7af
mathieuarthur <71327986+mathieuarthur@users.noreply.github.com>
2021-05-27 23:49:39 +0200

    change color podcast 3

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,7 @@
 	<Modal>
 		<Podcast color={"#E423F0"} title={"On m’appelait le fils du diable"} typo={"Patua One"} typoColor={"#FFCF2A"} typoSize={"5em"} clip={"./audio/podcast1.mp3"} />
 		<Podcast color={"#785BCA"} title={"On me comparait à un porcidou"} typo={"Recursive"} typoColor={"#55EFDB"} typoSize={"5em"} clip={"./audio/podcast2.mp3"} />
-		<Podcast color={"#5FC9FA"} title={"On m'a ruiné ma scolarité"} typo={"Abril Fatface"} typoColor={"#FFFFFE"} typoSize={"5em"} clip={"./audio/podcast3.mp3"} />
+		<Podcast color={"#5FC9FA"} title={"On m'a ruiné ma scolarité"} typo={"Abril Fatface"} typoColor={"#630D79"} typoSize={"5em"} clip={"./audio/podcast3.mp3"} />
 		<Podcast color={"#E9CECE"} title={"Trop maigre à leur yeux"} typo={"Days One"} typoColor={"#C92525"} typoSize={"5em"} clip={"./audio/podcast4.mp3"} />
 		<Podcast color={"#F4A6D0"} title={"Je me suis vengé"} typo={"Goblin One"} typoColor={"#0060FF"} typoSize={"5em"}  clip={"./audio/podcast5.mp3"} />
 		<Podcast color={"#3E4797"} title={"brown"} typo={"Newsreader"} typoColor={"#FF9000"} typoSize={"5em"} clip={"./audio/podcast.mp3"} />
